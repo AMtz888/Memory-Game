94fbd44b5dd8d0996260617e8b7f994ecc527a5d
Alexandra Martinez <amartz888@gmail.com>
2018-03-27 10:04:04 -0400

    Project completed

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ let numOfMoves = 0;
 let numMatched = 0;
 let firstClick = 0;
 let stars = 3;
-let endGame;
+let endGame = false;
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -40,29 +40,17 @@ function shuffle(array) {
     return array;
 }
 
+
 // Shuffles the cards and creates each card's HTML
 function createCardGrid() {
   cardList = shuffle(cardList);
   cardList.forEach(function(card) {
     $('.deck').append('<li class="card">' + card + '</li>');
     });
-
 }
 
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
+// Event listener for clicks, calls other functions to put the whole game together
 function playGame() {
   createCardGrid();
   $('.card').on('click', function() {
@@ -78,21 +66,18 @@ function playGame() {
 }
 
 
+// Checks cards for matches and whether the game is over
 function checkMatch() {
-  console.log("running checkMatch")
-  console.log(openCards[0].children().attr('class'), openCards[1].children().attr('class'))
   if (openCards[0].children().attr('class') === openCards[1].children().attr('class')) {
     openCards.forEach(function(card) {
       card.toggleClass('match animated tada');
       card.off('click');
       numMatched++
-      console.log("inside first if")
     });
   } else {
     openCards.forEach(function(card) {
       card.toggleClass('animated shake');
       setTimeout(function(){card.toggleClass('open show animated shake');}, 800);
-      console.log("inside else")
     });
   }
   openCards = [];
@@ -103,70 +88,81 @@ function checkMatch() {
   }
 }
 
-// Updates the number of moves and reduces star count on the page
+
+// Updates the number of moves and reduces star count on the webpage
 function updateGameInfo() {
-  $('.moves').html(numOfMoves);
+  $('.moves').html(String(numOfMoves) + ' Moves');
 
   if (numOfMoves >= 14 && numOfMoves <= 20) {
     $('#star3').attr('class', 'fa fa-star-o')
     stars = 2;
-    //return stars;
   } else if (numOfMoves > 20) {
     $('#star2').attr('class', 'fa fa-star-o')
     stars = 1;
-    //return stars;
   }
 }
 
+
 /* Adds a timer to the page
- * Resources to help me figure out the timer code:
+ * Resources used:
  * https://stackoverflow.com/questions/24155788/timer-to-be-displayed-on-a-webpage
  * https://www.w3schools.com/jsref/met_win_setinterval.asp
  */
- function startTimer() {
-   let min = 0;
-   let sec = 0;
-   let time;
-   if (firstClick === 1) {
-     time = setInterval(function() {
-       sec++;
-       if (sec > 59) {
-         sec = 0;
-         min++;
-       }
-     $('.timer').html((min > 9 ? min : '0'+min)+':'+(sec > 9 ? sec : '0'+sec));
-     if (endGame === true) {
-       clearInterval(time);
-     }
-     }, 1000);
-   }
- }
+function startTimer() {
+  let min = 0;
+  let sec = 0;
+  let time;
+  if (firstClick === 1) {
+    time = setInterval(function() {
+      sec++;
+      if (sec > 59) {
+        sec = 0;
+        min++;
+      }
+    $('.timer').html((min > 9 ? min : '0'+min)+':'+(sec > 9 ? sec : '0'+sec));
+    if (endGame === true) {
+      clearInterval(time);
+    }
+    }, 1000);
+  }
+}
 
+
+// reloads the page - used for restart button and modal
 function restart() {
   location.reload();
 }
 
+
+/* Functionality for the end-of-game popup
+ * Resources used:
+ * https://sabe.io/tutorials/how-to-create-modal-popup-box
+ * https://www.w3schools.com/howto/howto_css_modals.asp
+ */
 function theEnd() {
   let modal = document.querySelector('.modal');
   let closeButton = document.querySelector('.close-button');
   let yes = document.querySelector('#yes');
   let no = document.querySelector('#no');
 
-  $("#stars").text(String(stars));
+  // Adds star rating to modal text
+  $('#stars').text(String(stars));
 
   function toggleModal() {
-        modal.classList.toggle('show-modal');
-    }
+    modal.classList.toggle('show-modal');
+  }
   toggleModal();
 
-  closeButton.addEventListener("click", toggleModal);
-  yes.addEventListener("click", restart);
-  no.addEventListener("click", toggleModal);
-  console.log("running theEnd");
+  closeButton.addEventListener('click', toggleModal);
+  yes.addEventListener('click', restart);
+  no.addEventListener('click', toggleModal);
 }
 
+
+// Event listener for restart button
 $('.restart').on('click', function() {
   restart();
 });
+
 
 playGame();
